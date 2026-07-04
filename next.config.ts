20a6b58cbb0508b61f4a3787dfdcfdb17664f6ab
rafasixteen-laptop <rafaelrego16@gmail.com>
2026-07-04 21:30:01 +0100

    now?

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
 	output: "export",
 	images: {
 		unoptimized: true,
 	},
-	basePath: isGitHubPages ? "/calisthenics-alliance" : "",
-	assetPrefix: isGitHubPages ? "/calisthenics-alliance/" : undefined,
 };
 
 export default nextConfig;
