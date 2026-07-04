7b636d8360160c775a5f0f54b339e32b8c9d3455
rafasixteen-laptop <rafaelrego16@gmail.com>
2026-07-04 21:32:05 +0100

    ergth

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
 	output: "export",
 	images: {
 		unoptimized: true,
 	},
+	basePath: isGitHubPages ? "/calisthenics-alliance" : "",
+	assetPrefix: isGitHubPages ? "/calisthenics-alliance/" : undefined,
 };
 
 export default nextConfig;
